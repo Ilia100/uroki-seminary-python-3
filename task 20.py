991a7b8d57ef4acd4e53b6d3fe07c9211a56a4a8
ilia <ilia100@hotmail.com>
2023-03-24 00:47:48 +0300

    aded 20-2

--- task 20.py
+++ task 20.py
@@ -13,10 +13,10 @@
 # ноутбук
 #     12
 
-dictionary = {('A', 'E', 'I', 'O', 'U', 'L', 'N', 'S', 'T', 'R'): 1,('D', 'G'): 2, 
+dictionary = {('A', 'E', 'I', 'O', 'U', 'L', 'N', 'S', 'T', 'R'): 1,'DG': 2, 
 ('B', 'C', 'M', 'P'):3, ('F', 'H', 'V', 'W', 'Y'): 4, 'K':5, ('J','X'):8,} 
 
-a=input("Введите слово ")
+a=input("Введите слово ").upper()
 sum=0
 for i in range(len(a)):
     for keys, values in dictionary.items():
